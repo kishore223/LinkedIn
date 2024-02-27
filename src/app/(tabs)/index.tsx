e5c82af9345bb9b8fbf4f7ad6642aa037c6257b5
kishore223 <kishoremuruganmk@gmail.com>
2024-02-26 22:15:45 -0600

    Home Feed

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,22 +1,14 @@
-import { StyleSheet } from 'react-native';
-import { Text, View } from '../../components/Themed';
+import { FlatList } from 'react-native';
+import PostListItem from '../../components/PostListItem';
+import posts from '../../../assets/data/posts.json';
 
-export default function TabOneScreen() {
+export default function HomeFeedScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-    </View>
+    <FlatList
+      data={posts}
+      renderItem={({ item }) => <PostListItem post={item} />}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{gap: 10}}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
